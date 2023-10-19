97581d831dec58413568ee41df3d55a0733c00c9
huyech123 <148409033+huyech123@users.noreply.github.com>
2023-10-19 09:55:40 +0700

    Add files via upload
    card

---
+++ config.js
@@ -0,0 +1,9 @@
+const nameGirl = 'Phương Anh';
+const giftUrl = 'https://www.facebook.com/phuonganhilu';
+const eventName = 'Chúc Mừng 20-10';
+const titleCard = 'Tặng người ấy';
+const contentCard = 'Chúc Ngừi iu của tôi 20/10 tràn ngập niềm vui và những nụ cười. Mong điều đẹp nhất sẽ đến với em trong hôm nay và cả những ngày sau';
+
+const giftImage = 'phuonganh.png';
+const base64 = '';
+const giftImageBase64 = "data:image/png;base64, " + base64;
